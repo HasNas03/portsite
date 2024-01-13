a992dbda172e86cae2dc38d515241776a1eb7974
HasNas03 <109939049+HasNas03@users.noreply.github.com>
2024-01-13 16:13:29 -0500

    arrow

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -22,26 +22,26 @@ function Experience() {
           <Card name="FilePath Treemap Visualizer" paragraph="Python | pygame" image={treemap}/>
           {/* link to site suitability*/}
           <a href='https://github.com/HasNas03/Jupyter-notebooks/blob/main/Site%20Suitability%20Analysis%20Report%20-%20Personal.pdf' style={{ textDecoration: 'none', color: 'white' }} target="_blank">
-          <Card name="Site Suitability Analysis" paragraph="postgreSQL | QGIS | PostGIS" image={site}/>
+          <Card name="Site Suitability Analysis ↗" paragraph="postgreSQL | QGIS | PostGIS" image={site}/>
           </a>
           <Card name="ExerciseBuddy" paragraph="MongoDB | Express.js | Node.js JavaScript | React.js | HTML/CSS" image={exerciseImage}/>
           <Card name="Boggle Pro" paragraph="Java | JavaFX" image={boggle}/>
           <Card name="Tetris" paragraph="Java | JavaFX" image={tetris2}/>
           {/* link to to do*/}
           <a href='https://reactchecklist.netlify.app/' style={{ textDecoration: 'none', color: 'white' }} target="_blank">
-          <Card name="To-Do App" paragraph="JavaScript | React.js | HTML/CSS" image={todo}/>
+          <Card name="To-Do App ↗" paragraph="JavaScript | React.js | HTML/CSS" image={todo}/>
           </a>
           {/* link to emissions*/}
           <a href='https://github.com/HasNas03/Jupyter-notebooks/blob/main/emissions/Carbon%20Emissions.ipynb' style={{ textDecoration: 'none', color: 'white' }} target="_blank">
-          <Card name="CO2 Emission Data Analysis" paragraph="Python | pandas | Jupyter Notebook" image={carImage}/>
+          <Card name="CO2 Emission Data Analysis ↗" paragraph="Python | pandas | Jupyter Notebook" image={carImage}/>
           </a>
           {/* link to covid tableau*/}
           <a href='https://public.tableau.com/views/COVIDDashboard_17050443722110/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link' style={{ textDecoration: 'none', color: 'white' }} target="_blank">
-          <Card name="2020 COVID Cases Interactive Dashboard" paragraph="Tableau" image={covidImage}/>
+          <Card name="2020 COVID Cases Interactive Dashboard ↗" paragraph="Tableau" image={covidImage}/>
           </a>
           {/* link to house tableau*/}
           <a href='https://public.tableau.com/views/HouseSaleDashboard_17050002821300/HouseSaleDashboard?:language=en-US&:display_count=n&:origin=viz_share_link' style={{ textDecoration: 'none', color: 'white' }} target="_blank">
-          <Card name="Real Estate Interactive Dashboard" paragraph="Tableau" image={houseImage}/>
+          <Card name="Real Estate Interactive Dashboard ↗" paragraph="Tableau" image={houseImage}/>
           </a>
         </div>
       </div>
